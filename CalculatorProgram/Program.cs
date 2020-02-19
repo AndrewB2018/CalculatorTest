f5121969e431b727291df1ec5f0fbd88a8cd1865
Boynton <andy_boynton@hotmail.co.uk>
2020-02-20 08:26:13 +1100

    CalculatorTest - Dependency Injection - Add dependency injection to the calculator program for logging. Add a null logger that can be switched with the standard Nlogger class so that nothing is logged

--- CalculatorProgram/Program.cs
+++ CalculatorProgram/Program.cs
@@ -1,6 +1,8 @@
 ﻿using Calculator;
 using NLog;
 using System;
+using Unity;
+using Unity.Resolution;
 
 namespace CalculatorProgram
 {
@@ -8,8 +10,43 @@ namespace CalculatorProgram
     {
         static void Main(string[] args)
         {
-            //ILogger logger = new ILogger();
-            //SimpleCalculator calculator = new SimpleCalculator(logger);
+            try
+            { 
+                IUnityContainer container = new UnityContainer();
+
+                // Register
+                // Diagnostics interface that reports logs
+                container.RegisterFactory<ILogger>(l => LogManager.GetCurrentClassLogger());
+
+                // Dummy diagnostics interface that doesn't report anything
+                //container.RegisterFactory<ILogger>(l => LogManager.CreateNullLogger());
+
+                container.RegisterType<ISimpleCalculator, SimpleCalculator>();
+
+                // Resolve
+                ILogger logger = container.Resolve<ILogger>();
+
+                ISimpleCalculator calculator = container.Resolve<ISimpleCalculator>(new ResolverOverride[] { new ParameterOverride("logger", logger) });
+
+                // Add method
+                calculator.Add(0, 10);
+
+                // Divide method
+                calculator.Divide(9, 3);
+
+                // Subtract method
+                calculator.Subtract(10, 4);
+
+                // Multiply method
+                calculator.Multiply(5, 5);
+
+            }
+            catch (Exception e)
+            {
+                throw new Exception($"Unable to run simple calculator program", e);
+            }
+
         }
+
     }
 }
