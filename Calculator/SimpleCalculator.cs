151ee3fca330017e58b54797f420edb44a4033b5
Boynton <andy_boynton@hotmail.co.uk>
2020-02-19 16:35:36 +1100

    Initial Commit - Contains simple calculator interface and class

---
+++ Calculator/SimpleCalculator.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculator
+{
+    public class SimpleCalculator : ISimpleCalculator
+    {
+        public int Add(int start, int amount)
+        {
+            try
+            {
+                return start + amount;
+            }
+            catch(Exception e)
+            {
+                throw new Exception($"Unable to add values start:{start}, amount: {amount}");
+            }
+        }
+
+        public int Subtract(int start, int amount)
+        {
+            try
+            { 
+                return start - amount;
+            }
+            catch (Exception e)
+            {
+                throw new Exception($"Unable to subtract values start:{start}, amount: {amount}");
+            }
+        }
+
+        public int Multiply(int start, int amount)
+        {
+            try
+            { 
+                return start * amount;
+            }
+            catch (Exception e)
+            {
+                throw new Exception($"Unable to multiply values start:{start}, amount: {amount}");
+            }
+
+        }
+
+        public int Divide(int start, int amount)
+        {
+            try
+            { 
+                return start / amount;
+            }
+            catch (Exception e)
+            {
+                throw new Exception($"Unable to divide values start:{start}, amount: {amount}");
+            }
+        }
+    }
+}
