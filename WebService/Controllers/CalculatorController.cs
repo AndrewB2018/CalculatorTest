0971a88991ba17376f416eed469aca76d9e5bb28
Boynton <andy_boynton@hotmail.co.uk>
2020-02-20 12:33:50 +1100

    CalculatorTest - Add Web API project

---
+++ WebService/Controllers/CalculatorController.cs
@@ -0,0 +1,47 @@
+﻿using Calculator;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculatorController : ControllerBase
+    {
+
+
+        private readonly ISimpleCalculator _calculator;
+
+        public CalculatorController(ISimpleCalculator calculator)
+        {
+            _calculator = calculator;
+        }
+
+        [HttpGet]
+        [Route("add/{start:int}/{amount:int}")]
+        public int Add(int start = 0, int amount = 0)
+        {
+            return _calculator.Add(start, amount);
+        }
+
+        [HttpGet]
+        [Route("subtract/{start:int}/{amount:int}")]
+        public int Subtract(int start = 0, int amount = 0)
+        {
+            return _calculator.Subtract(start, amount);
+        }
+
+        [HttpGet]
+        [Route("divide/{start:int}/{amount:int}")]
+        public int Divide(int start = 0, int amount = 0)
+        {
+            return _calculator.Divide(start, amount);
+        }
+
+        [HttpGet]
+        [Route("multiply/{start:int}/{amount:int}")]
+        public int Multiply(int start = 0, int amount = 0)
+        {
+            return _calculator.Multiply(start, amount);
+        }
+    }
+}
